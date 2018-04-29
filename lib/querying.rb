b07d44f739a2d8246729f9fad66bbb0ec8b8f605
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-29 05:16:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,8 +26,8 @@ def select_series_title_with_most_human_characters
   """SELECT series.title FROM series
       INNER JOIN characters ON series.id=characters.series_id
       WHERE characters.species='human'
-      GROUP BY characters.species
-      ORDER BY characters.species DESC LIMIT 1;"""
+      GROUP BY series.id
+      ORDER BY COUNT(series.id) DESC LIMIT 1;"""
 end
 
 def select_character_names_and_number_of_books_they_are_in
